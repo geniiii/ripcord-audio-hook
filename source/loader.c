289bb212eead43283c0eb0ff1de180fbce66fad1
geni <me@geni.site>
2022-10-23 17:07:32 +0300

    This isn't C++

--- source/loader.c
+++ source/loader.c
@@ -1,4 +1,4 @@
-#define READ_VOICE_PACKET(name) u64 name(void* _this, void* decrypted_data, size_t original_size, u8 a4, u16 a5, u32 a6, u32 encrypted_size)
+#define READ_VOICE_PACKET(name) u64 name(void* this, void* decrypted_data, size_t original_size, u8 a4, u16 a5, u32 a6, u32 encrypted_size)
 typedef READ_VOICE_PACKET(ReadVoicePacketType);
 
 static ReadVoicePacketType* read_voice_packet_orig;
@@ -8,11 +8,11 @@ static READ_VOICE_PACKET(ReadVoicePacketHook) {
 		u16 size_in_dwords = data[3] | data[2] << 8;
 		if (size_in_dwords > 1) {
 			u32 size = size_in_dwords * 4 - 4;
-			return read_voice_packet_orig(_this, data + size, original_size - size_in_dwords - 1, a4, a5, a6, encrypted_size);
+			return read_voice_packet_orig(this, data + size, original_size - size_in_dwords - 1, a4, a5, a6, encrypted_size);
 		}
 	}
 
-	return read_voice_packet_orig(_this, decrypted_data, original_size, a4, a5, a6, encrypted_size);
+	return read_voice_packet_orig(this, decrypted_data, original_size, a4, a5, a6, encrypted_size);
 }
 
 static void LoadHooks() {
