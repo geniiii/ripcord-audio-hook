102297e7fa08b502f0afa8aeafa8b0729bcbde00
geni <me@geni.site>
2023-04-04 20:59:47 +0300

    Add readVoiceDataPacket hook Closes #5

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # ripcord-audio-hook (Linux)
-Fixes Ripcord sending and receiving an old (?) version of the IP discovery packet, causing it to get stuck on "Routing..." when attempting to join voice channels
+Fixes Ripcord:
+1. reading RTP header extensions larger than 8 bytes as voice data, causing garbled audio in voice chats
+2. sending and receiving an old (?) version of the IP discovery packet, causing it to get stuck on "Routing..." when attempting to join voice channels
 
 ## Usage
 For `hook.so`, see [Building](#Building). I don't distribute binaries as I have no idea what the proper way to do it is on Linux.
 1. Extract the Ripcord AppImage. (`./Ripcord-0.4.29-x86_64.AppImage --appimage-extract`)
 2. Copy `hook.so` to the directory where the AppImage was extracted (should be `squashfs-root`).
-3. Run Ripcord using `LD_PRELOAD=./hook.so ./Ripcord`. You should now be able to connect to voice channels.
+3. Run Ripcord using `LD_PRELOAD=./hook.so ./Ripcord`. You should now be able to connect to voice channels.  
+*If you get an error that looks something like "cannot open shared object file: No such file or directory" even after installing funchook, check your `LD_LIBRARY_PATH` or run `ldconfig`.*
 
 ## Building
-`gcc -shared -fPIC -ldl -o hook.so hook.c` (or the equivalent command for your compiler of choice)
+1. Build and install [funchook](https://github.com/kubo/funchook).
+2. `gcc -shared -fPIC -o hook.so hook.c -ldl -lfunchook` (or the equivalent command for your compiler of choice)
--- hook.c
+++ hook.c
@@ -7,12 +7,80 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <dlfcn.h>
+#include <funchook.h>
 
 typedef int64_t  i64;
+typedef int32_t  i32;
 typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+
+#define WRITE_DATAGRAM(name) i64 name(void* this, u8* data, i64 size, void* addr, u16 port)
+typedef WRITE_DATAGRAM(WriteDatagramType);
+static WriteDatagramType* write_datagram_orig;
+WRITE_DATAGRAM(_ZN10QUdpSocket13writeDatagramEPKcxRK12QHostAddresst) {
+    if (!write_datagram_orig) {
+        write_datagram_orig = dlsym(RTLD_NEXT, "_ZN10QUdpSocket13writeDatagramEPKcxRK12QHostAddresst");
+    }
+
+    // NOTE(geni): Ripcord appears to send what is likely an old version of this packet, which *only some* Discord servers seem to respond to.
+    //             See here: https://discord.com/developers/docs/topics/voice-connections#ip-discovery
+    if (size == 70) {
+        puts("Hooked writeDatagram");
+        u32 ssrc         = *((u32*) data);
+        data[0]          = 0;
+        data[1]          = 1;
+        data[2]          = 0;
+        data[3]          = 70;
+        ((u32*) data)[1] = ssrc;
+        return write_datagram_orig(this, data, 74, addr, port);
+    }
+
+    return write_datagram_orig(this, data, size, addr, port);
+}
+
+typedef struct {
+    u8* data;
+    u64 size_as_4;
+} DisVoiceDatagram;
+typedef struct {
+    DisVoiceDatagram datagram;
+    u32              _unk1;
+    u32              _unk2;
+    u32              _unk3;
+    u16              _unk4;
+    u8               _unk5;
+    u8               _padding;
+} SomeVoiceData;
+typedef struct {
+    void*          _unk1;
+    SomeVoiceData* some_voice_data_array;
+    SomeVoiceData* some_voice_data_array_unk;
+    u8*            internal_buf;
+    u64            buf_size;
+    u64            buf_size_in_dwords;
+} VoiceDataAccum;
+#define READ_VOICE_DATA_PACKET(name) void name(VoiceDataAccum* accum, u32 a2, const u8* a3, char* a4, u64 a5)
+typedef READ_VOICE_DATA_PACKET(ReadVoiceDataPacketType);
+static ReadVoiceDataPacketType* read_voice_data_packet_orig;
+READ_VOICE_DATA_PACKET(ReadVoiceDataPacketHook) {
+    read_voice_data_packet_orig(accum, a2, a3, a4, a5);
+
+    if (accum == NULL || accum->some_voice_data_array == NULL) {
+        return;
+    }
+
+    DisVoiceDatagram* dg = &accum->some_voice_data_array[-1].datagram;
+    if (dg->data[0] == 0xBE && dg->data[1] == 0xDE) {
+        u16 size_in_dwords = dg->data[3] | dg->data[2] << 8;
+        if (size_in_dwords > 1) {
+            --size_in_dwords;
+            dg->data += size_in_dwords * 4;
+            dg->size_as_4 -= size_in_dwords;
+        }
+    }
+}
 
 static void PatchByte(u8* addr, u8 value) {
     printf("Original %p: %x\n", addr, *addr);
@@ -31,28 +99,19 @@ static __attribute__((constructor)) void OnAttach(void) {
     PatchByte(receive_udp + 0x110, 8);
     PatchByte(receive_udp + 0x172, 72);
     puts("Patched bytes");
-}
 
-#define WRITE_DATAGRAM(name) i64 name(void* this, u8* data, i64 size, void* addr, u16 port)
-typedef WRITE_DATAGRAM(WriteDatagramType);
-static WriteDatagramType* write_datagram_orig;
-WRITE_DATAGRAM(_ZN10QUdpSocket13writeDatagramEPKcxRK12QHostAddresst) {
-    if (!write_datagram_orig) {
-        write_datagram_orig = dlsym(RTLD_NEXT, "_ZN10QUdpSocket13writeDatagramEPKcxRK12QHostAddresst");
+    read_voice_data_packet_orig = (ReadVoiceDataPacketType*) (0x400000 + 0x1557B0);
+    funchook_t* funchook        = funchook_create();
+
+    i32 err = funchook_prepare(funchook, (void**) &read_voice_data_packet_orig, ReadVoiceDataPacketHook);
+    if (err != 0) {
+        puts("Failed to prepare readVoiceDataPacket hook");
+        return;
     }
-
-    // NOTE(geni): Ripcord appears to send what is likely an old version of this packet, which *only some* Discord servers seem to respond to.
-    //             See here: https://discord.com/developers/docs/topics/voice-connections#ip-discovery
-    if (size == 70) {
-        u32 ssrc         = *((u32*) data);
-        data[0]          = 0;
-        data[1]          = 1;
-        data[2]          = 0;
-        data[3]          = 70;
-        ((u32*) data)[1] = ssrc;
-        puts("Hooked writeDatagram");
-        return write_datagram_orig(this, data, 74, addr, port);
+    err = funchook_install(funchook, 0);
+    if (err != 0) {
+        puts("Failed to install readVoiceDataPacket hook");
+        return;
     }
-
-    return write_datagram_orig(this, data, size, addr, port);
+    puts("Installed readVoiceDataPacket hook");
 }
